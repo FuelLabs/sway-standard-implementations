1a34ae56faef65de36bf2e37f4a80497899de588
K1-R1 <k1r1.devwork@gmail.com>
2024-09-05 19:00:52 +0100

    fix ci

--- tests/src/src14/owned_proxy/tests/functions/proxy_target.rs
+++ tests/src/src14/owned_proxy/tests/functions/proxy_target.rs
@@ -4,18 +4,19 @@ mod success {
         proxy_abi_calls::{initialize_proxy, proxy_target},
         test_helpers::setup,
     };
-
+    use fuels::types::errors::Result;
     #[tokio::test]
-    async fn returns_initialized_target() {
-        let (_deployer, owner1, _owner2, initial_target_id) = setup().await;
+    async fn returns_initialized_target() -> Result<()> {
+        let (_deployer, owner1, _owner2, initial_target_id) = setup().await?;
 
-        assert_eq!(proxy_target(&owner1.proxy_contract).await.value, None);
+        assert_eq!(proxy_target(&owner1.proxy_contract).await?.value, None);
 
-        initialize_proxy(&owner1.proxy_contract).await;
+        initialize_proxy(&owner1.proxy_contract).await?;
 
         assert_eq!(
-            proxy_target(&owner1.proxy_contract).await.value,
+            proxy_target(&owner1.proxy_contract).await?.value,
             Some(initial_target_id)
         );
+        Ok(())
     }
 }
